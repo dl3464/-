8af5341666937efd2c9b280d4a709b4607012609
dl <1830481071@qq.com>
2021-04-23 14:26:16 +0800

    commit in 4.23

--- 2.py
+++ 2.py
@@ -6,8 +6,15 @@ import unittest
 class NewVisitorTest(unittest.TestCase):#(1)
     def setUp(self):#(3)
         self.brower = webdriver.Firefox()
+
     def tearDown(self):#(3)
         self.brower.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table=self.brower.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):#(2)
         # Edith has heard about a cool new online to-do app.She goes
         # to check out its homepage
@@ -30,11 +37,7 @@ class NewVisitorTest(unittest.TestCase):#(1)
         # “1:Buy peacock feathers as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(10)
-
-        table=self.brower.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.check_for_row_in_list_table("1:Buy peacock feathers")
 
         #there is still a text box inviting her to add another items.She
         #enter "Use peacock feathers to make a fly"(Edith is very methodical)
@@ -44,14 +47,8 @@ class NewVisitorTest(unittest.TestCase):#(1)
         time.sleep(1)
 
         #the page updates again,and now shows both items on her list
-        table=self.brower.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2:User peacock feather to make a fly',
-            [row.text for row in rows]
-        )
-
+        self.check_for_row_in_list_table("1:Buy peacock feathers")
+        self.check_for_row_in_list_table("2:Use peacock feathers to make a fly")
         #Edith wonders whether the site will remaber her list.Then she sees
         #that the site has generated a unique URL for her -- there is some
         #explanatory text to that effect
